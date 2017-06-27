cd9334582580c7c1d3fc1dbb74fe3331615135ff
Witek85 <witold.szklarski@gmail.com>
2017-06-27 23:06:36 +0200

    polyfill

--- script-compiled.js
+++ script-compiled.js
@@ -1,5 +1,7 @@
 "use strict";
 
+// babel - transpilacja
+
 function sumTwoNumbers() {
 	var x = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 1;
 	var y = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 2;
@@ -8,3 +10,14 @@ function sumTwoNumbers() {
 }
 
 console.log(sumTwoNumbers());
+
+// polyfill, np.
+// https://github.com/paulmillr/es6-shim
+// można pobrać i załączyć przed skryptem
+
+var name = "main-header";
+var arr = [1, 2, 3, 4];
+
+// metoda - sprawdza czy ciąg znaków jest w zmiennej name
+console.log(name.includes('main'));
+console.log(arr.includes(1));
--- script.js
+++ script.js
@@ -1,5 +1,18 @@
+// babel - transpilacja
+
 function sumTwoNumbers(x = 1, y = 2) {
 	return x + y;
 }
 
 console.log(sumTwoNumbers());
+
+// polyfill, np.
+// https://github.com/paulmillr/es6-shim
+// można pobrać i załączyć przed skryptem
+
+let name = "main-header";
+const arr = [1,2,3,4];
+
+// metoda - sprawdza czy ciąg znaków jest w zmiennej name
+console.log(name.includes('main'));
+console.log(arr.includes(1));
